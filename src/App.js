1477e1c8386fcad5731e5c7879c10ec0119be713
jonnebonde <jonnebonde@gmail.com>
2024-05-17 23:06:09 +0200

    added basic setup of react router and layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React, { Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout";
+import "./Styles/global.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<h1>Hello</h1>} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
