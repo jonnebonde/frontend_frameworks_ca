8b50ef1b27dacd40b0f251419b1b61a2a847c6c6
jonnebonde <jonnebonde@gmail.com>
2024-05-18 20:24:08 +0200

    installed prettier and run a format

--- src/Components/Layout/Header/Links/index.jsx
+++ src/Components/Layout/Header/Links/index.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-
 function Links() {
   return (
     <div>
-      <NavLink to="/" >
-        Home
-      </NavLink>
-      <NavLink to="/about" >
-        About
-      </NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
     </div>
   );
 }
