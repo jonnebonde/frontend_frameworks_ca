bac950e61b05f7fdc44d8a49dd452db909e37a22
jonnebonde <jonnebonde@gmail.com>
2024-05-23 21:04:15 +0200

    fixed hamburger closing on click

--- src/Components/Layout/Header/Nav/CartIcon/index.jsx
+++ src/Components/Layout/Header/Nav/CartIcon/index.jsx
@@ -1,13 +1,18 @@
 import { IoCartOutline } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
 /* import { IoCart } from "react-icons/io5"; */
 
-function CartIcon() {
+function CartIcon({ setIsOpen }) {
   return (
-    <NavLink to="/cart">
+    <NavLink to="/cart" tabIndex="1" onClick={() => setIsOpen(false)}>
       <IoCartOutline size={40} />
     </NavLink>
   );
 }
+
+CartIcon.propTypes = {
+  setIsOpen: PropTypes.func.isRequired,
+};
 
 export default CartIcon;
--- src/Components/Layout/Header/Nav/Logo/index.jsx
+++ src/Components/Layout/Header/Nav/Logo/index.jsx
@@ -1,7 +1,16 @@
+import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
-function MainLogo() {
-  return <NavLink to="/">WeGotIt</NavLink>;
+function MainLogo({ setIsOpen }) {
+  return (
+    <NavLink to="/" onClick={() => setIsOpen(false)}>
+      WeGotIt
+    </NavLink>
+  );
 }
+
+MainLogo.propTypes = {
+  setIsOpen: PropTypes.func.isRequired,
+};
 
 export default MainLogo;
