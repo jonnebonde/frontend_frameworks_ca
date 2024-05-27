78bdfd38cd6bf4877bf00794adb08499485adbda
jonnebonde <jonnebonde@gmail.com>
2024-05-27 21:28:24 +0200

    minor changes to cartIcon

--- src/Components/Layout/Header/Nav/CartIcon/index.styles.jsx
+++ src/Components/Layout/Header/Nav/CartIcon/index.styles.jsx
@@ -7,10 +7,12 @@ const CartIconContainer = styled.div`
 const CartIconText = styled.div`
   position: absolute;
   background-color: var(--color-button);
+  opacity: 0.9;
   padding: 2px;
   border-radius: 50%;
   top: 5px;
-  right: 10px;
+  right: 8px;
+  font-size: 1.1rem;
 `;
 
 export { CartIconContainer, CartIconText };
