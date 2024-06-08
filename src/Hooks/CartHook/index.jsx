0d4c121f2e9e148261bea755e68ca1b441c9e71e
jonnebonde <jonnebonde@gmail.com>
2024-06-08 19:43:54 +0200

    made a custom hook for localstorage instead of using a an package.

--- src/Hooks/CartHook/index.jsx
+++ src/Hooks/CartHook/index.jsx
@@ -1,13 +1,16 @@
-import { useLocalStorage } from "@uidotdev/usehooks";
 import PropTypes from "prop-types";
 import { createContext, useEffect, useReducer, useMemo } from "react";
 import cartReducer from "../../Components/Cart/CartReducer";
+import { useLocalStorageState } from "../LocalStorageHook";
 
 export const CartContext = createContext();
 const initialCartState = { cart: [], total: 0, totalItems: 0 };
 
 export const CartProvider = ({ children }) => {
-  const [storedCart, setStoredCart] = useLocalStorage("cart", initialCartState);
+  const [storedCart, setStoredCart] = useLocalStorageState(
+    "cart",
+    initialCartState,
+  );
 
   const [cart, dispatch] = useReducer(
     cartReducer,
