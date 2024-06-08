5cf16b5274c82d60cd98ebe8fa2290aeb19870b5
jonnebonde <jonnebonde@gmail.com>
2024-06-08 20:05:25 +0200

    added a custom local storage hook instead of using an library

--- src/Hooks/CartContext/index.jsx
+++ src/Hooks/CartContext/index.jsx
@@ -1,13 +1,16 @@
-import { useLocalStorage } from "@uidotdev/usehooks";
 import PropTypes from "prop-types";
 import { createContext, useEffect, useReducer, useMemo } from "react";
 import cartReducer from "../CartReducer";
+import { useLocalStorageState } from "../LocalStorage";
 
 export const CartContext = createContext();
 const initialCartState = { cart: [], total: 0, totalItems: 0 };
 
 export const CartProvider = ({ children }) => {
-  const [storedCart, setStoredCart] = useLocalStorage("cart", initialCartState);
+  const [storedCart, setStoredCart] = useLocalStorageState(
+    "cart",
+    initialCartState,
+  );
 
   const [cart, dispatch] = useReducer(
     cartReducer,
@@ -19,7 +22,7 @@ export const CartProvider = ({ children }) => {
   }, [cart, setStoredCart]);
 
   // I was recommened by sonarcloud to use useMemo to avoid unnecessary re-renders
-  // But im not sure how i can check the difference with or without useMemo
+  // But im not sure if it is necessary in this case because i cant see any re-renders happening.
 
   const contextValue = useMemo(() => ({ cart, dispatch }), [cart, dispatch]);
 
